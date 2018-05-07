a3ed0ebff31b2b9100f8d17451ce4063b53087aa
Renato Benatti <renato.benatti@edu.unipar.br>
2018-05-07 20:48:21 -0300

    ToolBar Adicionado

--- app/src/main/java/com/example/renato/myapplication/MainActivity.java
+++ app/src/main/java/com/example/renato/myapplication/MainActivity.java
@@ -1,25 +1,50 @@
 package com.example.renato.myapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
 public class MainActivity extends Activity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = (TextView) findViewById(R.id.v1);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle( "Testando" );
+
+         //Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(getResources().getDrawable(R.drawable.profile))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+//Now create your drawer and pass the AccountHeader.Result
 
         new DrawerBuilder().withActivity(this).build();
         //if you want to update the items at a later time it is recommended to keep it in a variable
@@ -27,23 +52,25 @@ public class MainActivity extends Activity {
         SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName("config");
 
 //create the drawer and remember the `Drawer` result object
-        Drawer result = new DrawerBuilder()
-                .withActivity(this)
-                //.withToolbar(toolbar)
-                .addDrawerItems(
-                        item1,
-                        new DividerDrawerItem(),
-                        item2,
-                        new SecondaryDrawerItem().withName("Nova Linha")
-                )
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        textView.setText("Você clicou em: "+ ((Nameable) drawerItem).getName().getText(MainActivity.this));
-                        return false;
-                    }
-                })
-                .build();
+        DrawerBuilder drawerBuilder = new DrawerBuilder();
+        drawerBuilder.withActivity( this );
+        drawerBuilder.withAccountHeader(headerResult);
+        drawerBuilder.withToolbar(toolbar);
+        drawerBuilder.addDrawerItems(
+                item1,
+                new DividerDrawerItem(),
+                item2,
+                new SecondaryDrawerItem().withName( "Nova Linha" )
+        );
+        drawerBuilder.withOnDrawerItemClickListener( new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                return false;
+            }
+        } );
+        Drawer result = drawerBuilder.build();
 
     }
+
 }
